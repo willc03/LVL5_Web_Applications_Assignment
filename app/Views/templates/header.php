5f8ea646a1f295307e1630f8d2d495634d532916
Will <corkillw@gmail.com>
2022-11-20 20:44:02 +0000

    Adds basic header to begin with understanding CodeIgniter

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -2,6 +2,14 @@
 <html>
     <head>
         <title>CodeIgniter</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
     </head>
     <body>
-        <h1> <?php esc($title) ?> </h1>
+        <div id="header">
+            <h2 id="company_title">Furness Golf Club</h2>
+            <nav id="navigation">
+                <?php foreach ($nav_pages as $nav_button) { ?>
+                    <a href="<?php echo $nav_button["url"]; ?>"><?php echo $nav_button["btn_title"]; ?></a>
+                <?php } ?>
+            </nav>
+        </div>
