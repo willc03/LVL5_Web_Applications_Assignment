fa6329126d0e12c50b1bfdb5ea7121f5f422bf13
Will <corkillw@gmail.com>
2022-11-23 22:29:19 +0000

    Remove bootstrap due to preference to write CSS from scratch

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,14 +1,6 @@
-        <div class="footer fixed-bottom">
-            <div class="container-auto bg-light">
-                <div class="row">
-                    <div class="col ms-3">
-                        <p class="lead">FGC</p>
-                    </div>
-                </div>
-                <em>&copy; 2022</em>
-            </div>
         </div>
-        <!-- Again use the Bootstrap CDN to get the JavaScript files. -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <div id="footer">
+            <em>&copy; 2022</em>
+        </div>
     </body>
 </html>
--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -3,28 +3,22 @@
     <head>
         <title>Furness Golf Club - <?php echo $title; ?></title>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="icon" href="/assets/favicon.ico">
-        <!-- Use the Bootstrap Content Delivery Network (CDN) to download the CSS files. -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <!-- Enables responsive features -->
+        <link rel="icon" href="assets/favicon.ico"> <!-- A custom icon will be used for this web application -->
+        <link rel="stylesheet" type="text/css" href="assets/style.css"> <!-- Custom-written CSS will be used, as it is easier to customise -->
     </head>
     <body>
         <div id="header">
-            <nav class="navbar navbar-expand-lg bg-light"> <!-- Bootstrap framework has been used to created navigation bar -->
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/">Furness Golf Club</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle Navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <?php foreach ($nav_pages as $nav_button) { ?>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="<?php echo $nav_button["url"]; ?>"><?php echo $nav_button["btn_title"]; ?></a>
-                                </li>
-                            <?php } ?>
-                        </ul>
-                    </div>
-                </div>
+            <nav id="nav" class="container">
+                <a id="nav" class="title" href="/">Furness Golf Club</a>
+                <ul id="nav" class="buttons">
+                    <?php foreach ($nav_pages as $nav_button) { ?>
+                        <li>
+                            <a id="nav" class="button" href="<?php echo $nav_button["url"]; ?>"><?php echo $nav_button["btn_title"]; ?></a>
+                        </li>
+                    <?php } ?>
+                </ul>
             </nav>
         </div>
+        <div id="main">
+        
