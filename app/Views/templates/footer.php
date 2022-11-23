ae3e31a55323a0fe183b3d93002e541386f0fb00
Will <corkillw@gmail.com>
2022-11-23 11:52:18 +0000

    Add bootstrap and a basic footer

--- app/Views/templates/footer.php
+++ app/Views/templates/footer.php
@@ -1,3 +1,14 @@
-        <em>&copy; 2021</em>
+        <div class="footer fixed-bottom">
+            <div class="container-auto bg-light">
+                <div class="row">
+                    <div class="col ms-3">
+                        <p class="lead">FGC</p>
+                    </div>
+                </div>
+                <em>&copy; 2022</em>
+            </div>
+        </div>
+        <!-- Again use the Bootstrap CDN to get the JavaScript files. -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
