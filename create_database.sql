956e18ebb5e38f3af8f9d0374e9a8bd5c766ca35
willcorkill13 <corkillw@gmail.com>
2023-03-22 11:51:28 +0000

    Fix error messages by checking for items before executing code

--- create_database.sql
+++ create_database.sql
@@ -4,14 +4,14 @@ and data that should be added.
 
 This script should be run by the root user of a database.
 */
-CREATE DATABASE G20973951_CO2717;
+CREATE DATABASE IF NOT EXISTS G20973951_CO2717;
 USE G20973951_CO2717;
 
 /*
 Create tables to hold data
 */
 
-CREATE TABLE Users (
+CREATE TABLE IF NOT EXISTS Users (
     UserId int NOT NULL AUTO_INCREMENT,
     Email varchar(50) NOT NULL,
     Password varchar(255) NOT NULL, -- The password has an extended maximum length for hashing and security reasons.
@@ -29,22 +29,22 @@ users have different permissions (such as not being able to)
 drop tables.
 */
 
--- Base users
-CREATE USER 'G20973951_CO2717_user'@'localhost' IDENTIFIED BY 'db_usr_pwd_Pa55word';
-GRANT SELECT ON G20973951_CO2717.Users TO 'G20973951_CO2717_user'@'localhost' WITH GRANT OPTION;
+-- Base users, this is for very basic database interactions only. This user will not be permitted to delete data.
+CREATE USER IF NOT EXISTS 'G20973951_CO2717_user'@'localhost' IDENTIFIED BY 'db_usr_pwd_Pa55word';
+GRANT SELECT, CREATE ON G20973951_CO2717.Users TO 'G20973951_CO2717_user'@'localhost' WITH GRANT OPTION;
 
 -- Junior golfers
-CREATE USER 'G20973951_CO2717_junior_member'@'localhost' IDENTIFIED BY 'db_usr_pwd_Pa55word';
+CREATE USER IF NOT EXISTS 'G20973951_CO2717_junior_member'@'localhost' IDENTIFIED BY 'db_usr_pwd_Pa55word';
 
 -- Social members (non-playing)
-CREATE USER 'G20973951_CO2717_social_member'@'localhost' IDENTIFIED BY 'db_usr_pwd_Pa55word';
+CREATE USER IF NOT EXISTS 'G20973951_CO2717_social_member'@'localhost' IDENTIFIED BY 'db_usr_pwd_Pa55word';
 
 -- Full playing members
-CREATE USER 'G20973951_CO2717_full_member'@'localhost' IDENTIFIED BY 'db_usr_pwd_Pa55word';
+CREATE USER IF NOT EXISTS 'G20973951_CO2717_full_member'@'localhost' IDENTIFIED BY 'db_usr_pwd_Pa55word';
 
 -- Staff
-CREATE USER 'G20973951_CO2717_staff'@'localhost' IDENTIFIED BY 'db_usr_pwd_Pa55word';
+CREATE USER IF NOT EXISTS 'G20973951_CO2717_staff'@'localhost' IDENTIFIED BY 'db_usr_pwd_Pa55word';
 
 -- Admin
-CREATE USER 'G20973951_CO2717_admin'@'localhost' IDENTIFIED BY 'db_usr_pwd_Pa55word';
+CREATE USER IF NOT EXISTS 'G20973951_CO2717_admin'@'localhost' IDENTIFIED BY 'db_usr_pwd_Pa55word';
 GRANT ALL PRIVILEGES ON G20973951_CO2717.* TO 'G20973951_CO2717_admin'@'localhost' WITH GRANT OPTION;
