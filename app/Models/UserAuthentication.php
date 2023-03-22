df1162b521a852477b5dbc9588fd1f2f86854295
willcorkill13 <corkillw@gmail.com>
2023-03-22 11:27:00 +0000

    Fix a bug where POST data was incorrectly used

--- app/Models/UserAuthentication.php
+++ app/Models/UserAuthentication.php
@@ -6,15 +6,15 @@ use CodeIgniter\Model;
 class UserAuthentication extends Model
 {
     protected $table = 'User';
-    public function ResolveUserFromEmail($email)
+    private function ResolveUserFromEmail($email): bool
     {
         $db = db_connect(); // Connect to the database using the default privileges.
 
         $builder = $db->table('Users'); // Set the table to 'Users'
-        $builder->getWhere(['Email'=>$_POST['email']]); // Set the query to only return results where the email is matched.
+        $results = $builder->getWhere(['Email'=>$email]); // Set the query to only return results where the email is matched.
 
         $db->close(); // Close the active database connection
-        return $builder->countAllResults(false) == 1; // Return true if the result is 1, indicating the user exists.
+        return count($results->getResultArray()) == 1; // Return true if the result is 1, indicating the user exists.
     }
 
     public function checkUserPassword($email, $password)
