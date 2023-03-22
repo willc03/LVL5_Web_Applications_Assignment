6170aaa0760b731b34a1eeb8d19215a54a9b6e90
willcorkill13 <corkillw@gmail.com>
2023-03-22 21:46:11 +0000

    Tidy up file and add documentation comments

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -10,28 +10,17 @@ namespace App\Controllers;
 
 class Login extends BaseController
 {
-
     // GET Request Handling
     public function view($page = 'login')
     {
         if (! is_file(APPPATH . 'Views/pages/dynamic/loginPages/' . $page . '.php')) {
-            // Whoops, we don't have a page for that!
-            throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
+            throw new \CodeIgniter\Exceptions\PageNotFoundException($page); // No page is available.
         }
-
-        /*
-            The title of the page will be passed in to the page to be used in
-            the title bar of the browser.
-        */
+        // Set the title of the page.
         $data['title'] = ucfirst($page);
-
-        /*
-            As CodeIgniter provides different functionality to  add different
-            views dynamically  in the same page.  At the time of writing, the
-            views added to the page will be a global header, followed by  the
-            page content, and then a global footer. 
-        */
+        // Get the navigation pages.
         $data['nav_pages'] = $this->getNavigationBarPages();
+        // Add the page content
         return view('templates/header', $data)
             . view('pages/dynamic/loginPages/' . $page)
             . view('templates/footer');
@@ -92,8 +81,9 @@ class Login extends BaseController
 
     public function logout()
     {
-        $this->session->destroy(); // Remove all session data. The user is n-
-                                   // ow logged out.
+        // Destroy the session.
+        $this->session->destroy();
+        // Redirect to the home page.
         return redirect()->to(site_url('/'));
     }
 }
