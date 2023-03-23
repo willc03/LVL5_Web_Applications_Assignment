c6edbe4fca47af51b433433ee522025242e473b0
willcorkill13 <corkillw@gmail.com>
2023-03-23 10:41:07 +0000

    Add an error message for if someone tries to access member areas

--- app/Views/pages/static/home.php
+++ app/Views/pages/static/home.php
@@ -1,1 +1,7 @@
+<?php if (isset($_GET["error"]) && $_GET["error"] == "not_logged_in") {?>
+    <div class="message_box error">
+        <h2>Error: Members Only Area</h2>
+        <p><a href="<?php echo site_url("/account/login"); ?>">Log in</a> as a member to access member-only areas.</p>
+    </div>
+<?php } ?>
 <h2>home page</h2>
