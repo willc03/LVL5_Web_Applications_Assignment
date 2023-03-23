456a92cca4316faa81238925a7a90bf6fd22ff62
willcorkill13 <corkillw@gmail.com>
2023-03-23 09:37:52 +0000

    Move static templates

---
+++ app/Views/templates/staticTemplates/footer.php
@@ -0,0 +1,20 @@
+            </div>
+        </div>
+        <div id="footer">
+            <div id="address">
+                <h2>Furness Golf Club</h2>
+                <p>Central Drive, Walney</p>
+                <p>Barrow-in-Furness</p>
+                <p>LA14 3HZ</p>
+            </div>
+            <div id="contact">
+                <h2>Contact Us</h2>
+                <p>Email: <a href="mailto:info@furnessgolfclub.co.uk">info@furnessgolfclub.co.uk</a></p>
+                <p>Telephone: <a href="tel:01229471232">01229 471232</a></p>
+            </div>
+            <div id="map">
+                <iframe loading="eager" src="https://maps.google.com/maps?q=Furness%20Golf%20Club&amp;t=m&amp;z=10&amp;output=embed&amp;iwloc=near" title="Furness Golf Club"></iframe>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ app/Views/templates/staticTemplates/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo 'Furness Golf Club - ' . esc($title); ?></title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <!-- Enables responsive features -->
+        <link rel="icon" href="assets/favicon.ico"> <!-- A custom icon will be used for this web application -->
+        <link rel="stylesheet" type="text/css" href="/assets/style.css"> <!-- Custom-written CSS will be used, as it is easier to customise -->
+    </head>
+    <body>
+        <div id="footerWrap">
+            <div id="header">
+                <nav id="nav" class="container">
+                    <a id="nav" class="title" href="/">Furness Golf Club</a>
+                    <ul id="nav" class="buttons">
+                        <?php foreach ($nav_pages as $nav_button) { ?>
+                            <li>
+                                <a id="nav" class="button" href="<?php echo $nav_button["url"]; ?>"><?php echo $nav_button["btn_title"]; ?></a>
+                            </li>
+                        <?php } ?>
+                    </ul>
+                </nav>
+            </div>
+            <div id="main" class="flex_container">
+        
