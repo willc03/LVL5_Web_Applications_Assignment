27d4b0cb4293c60ac7645f9d71b6900958e177c8
willcorkill13 <corkillw@gmail.com>
2023-03-29 09:31:37 +0100

    Add comments

--- app/Controllers/Members.php
+++ app/Controllers/Members.php
@@ -8,20 +8,16 @@ class Members extends BaseController
     {
 
         $isLoggedIn = $this->session->get('isLoggedIn');
+        // Redirect the user if they are not logged in as a member or visitor
         if (!$isLoggedIn)
         {
             return redirect()->to(site_url('/home?error=not_logged_in'));
         }
-
+        // Set up the navigation pages, title, and return the views.
         $data['nav_pages'] = $this->getNavigationBarPages();
         $data['title'] = 'Members Portal';
         return view('templates/memberTemplates/header', $data)
             .view('pages/dynamic/memberPages/portalHome')
             . view('templates/memberTemplates/footer');
-    }
-
-    public function test()
-    {
-        echo 'test';
     }
 }
