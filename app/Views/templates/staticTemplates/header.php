136daf87109d511807766160183c5bb84b7fea90
willcorkill13 <corkillw@gmail.com>
2023-04-14 23:25:34 +0100

    Restructure headings

--- app/Views/templates/staticTemplates/header.php
+++ app/Views/templates/staticTemplates/header.php
@@ -6,13 +6,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"> <!-- Enables responsive features -->
         <link rel="icon" href="assets/favicon.ico"> <!-- A custom icon will be used for this web application -->
         <link rel="stylesheet" type="text/css" href="/assets/style.css"> <!-- Custom-written CSS will be used, as it is easier to customise -->
+        <script src="http://maps.googleapis.com/maps/api/js"></script>
     </head>
     <body>
         <div id="footerWrap">
             <div id="header">
-                <nav id="nav" class="container">
-                    <a id="nav" class="title" href="/">Furness Golf Club</a>
-                    <ul id="nav" class="buttons">
+                <a id="title" href="/">Furness Golf Club</a>
+                <nav id="navigation">
+                    <ul>
                         <?php foreach ($nav_pages as $nav_button) { ?>
                             <li>
                                 <a id="nav" class="button" href="<?php echo $nav_button["url"]; ?>"><?php echo $nav_button["btn_title"]; ?></a>
