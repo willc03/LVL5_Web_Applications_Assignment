54232cf0eba081f2d0514bcadbe4d94ed05f1b9f
willcorkill13 <corkillw@gmail.com>
2023-04-17 20:42:15 +0100

    Load Google Maps API on about for location

---
+++ app/Views/pages/static/about.php
@@ -0,0 +1,12 @@
+<div id="fgcMap" style="width:500px;height:500px;"></div>
+<script>
+    function initialise() {
+        var mapProp = {
+            center:new google.maps.LatLng(54.106531461438195, -3.257412809651328),
+            zoom:17,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+        };
+        var map=new google.maps.Map(document.getElementById("fgcMap"),mapProp);
+    }
+    addEventListener('load', initialise);
+</script>
