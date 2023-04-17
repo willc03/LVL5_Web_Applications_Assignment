fcd530fbfd71305c0f71c9b36a53646b3bb8bf2a
willcorkill13 <corkillw@gmail.com>
2023-04-17 16:00:17 +0100

    Add access denied for unauthorised users

--- app/Controllers/Members.php
+++ app/Controllers/Members.php
@@ -7,11 +7,15 @@ class Members extends BaseController
     public function index()
     {
 
-        $isLoggedIn = $this->session->get('isLoggedIn');
+        $isLoggedIn = session()->get('isLoggedIn');
         // Redirect the user if they are not logged in as a member or visitor
         if (!$isLoggedIn)
         {
             return redirect()->to(site_url('/home?error=not_logged_in'));
+        }
+        elseif (session()->get('privilegeLevel') < 2)
+        {
+            return redirect()->to(site_url('/home?error=access_denied'));
         }
         // Set up the navigation pages, title, and return the views.
         $data['nav_pages'] = $this->getNavigationBarPages();
