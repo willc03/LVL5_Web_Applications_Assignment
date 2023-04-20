89e20c9f39f9fb949d29c8e3fd66f8d0169ddd2c
willcorkill13 <corkillw@gmail.com>
2023-04-20 16:45:49 +0100

    Begin create booking page

--- app/Views/pages/golf/booking_create.php
+++ app/Views/pages/golf/booking_create.php
@@ -1,1 +1,13 @@
-<?php
+<h1>Create a booking</h1>
+<div class="info_container">
+    <h3 class="title">Date:</h3>
+    <p><?php echo $_GET["date"]; ?></p>
+</div>
+<div class="info_container">
+    <h3 class="title">Time:</h3>
+    <p><?php echo $_GET["time"]; ?></p>
+</div>
+
+<div class="player_selector">
+    <h3></h3>
+</div>
