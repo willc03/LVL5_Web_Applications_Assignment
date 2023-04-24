ca03e7b944c3942e23aabfe7d6ef153f5957e458
Will <corkillw@gmail.com>
2023-04-24 22:19:11 +0100

    Add methods to get timeset info

--- app/Models/GolfManagement.php
+++ app/Models/GolfManagement.php
@@ -6,14 +6,11 @@ use CodeIgniter\Database\RawSql;
 
 class GolfManagement extends Model
 {
-    /**
+    /*
      * This method will be used to get the times on a day when golf can be booked according to the database.
      * For note, the first row of the table is the default data that will be used if no times exist.
-     *
-     * @param $date
-     * @return array
      */
-    public function GetTimesForDate($date): array
+    public function GetTimesForDate($date)
     {
         $db = db_connect(); // Connect to the database using the default credentials
         $builder = $db->table("GolfTimes"); // Set the active table to GolfTimes
@@ -42,6 +39,60 @@ class GolfManagement extends Model
             $current_time += $elapsed_seconds;
         }
         return $availableTimes;
+    }
+
+    public function GetAllTimes()
+    {
+        $startTime = strtotime('12:00 AM');
+        $endTime = strtotime('11:50 PM');
+
+        $current = $startTime;
+        $times = [];
+        while ($current <= $endTime) {
+            $times[] = date('h:i A', $current);
+            $current += 600; // 10 minutes in seconds
+        }
+        return $times;
+    }
+
+    public function GetAllIncrements()
+    {
+        $increments = array();
+        for ($i = 300; $i <= 3600; $i += 300) {
+            $increments[] = gmdate("H:i:s", $i);
+        }
+        return $increments;
+    }
+
+    public function GetTimeSettingsOnDate($date)
+    {
+        $db = db_connect(); // Connect to the database using the default credentials
+        $builder = $db->table("GolfTimes"); // Set the active table to GolfTimes
+        $escapedDate = $db->escape($date); // Prevents SQL injection
+        $results = $builder->getWhere("$escapedDate BETWEEN StartDate AND EndDate"); // Query results where the row's dates contain the date provided
+        // Return the relevant results to the player
+        $availableTimes = array();
+        if (!$results || count($results->getResultArray()) == 0)
+        {
+            // There were no set times found
+            $default_results = $builder->getWhere("TimeId = 1")->getResultArray();
+            $default_time_settings = $default_results[0];
+        }
+        else
+        {
+            // Times found
+            $default_time_settings = $results->getResultArray()[count($results->getResultArray())-1];
+        }
+        $start_time = strtotime($default_time_settings["StartTime"]);
+        $end_time = strtotime($default_time_settings["EndTime"]);
+        $current_time = $start_time;
+        while ($current_time <= $end_time) {
+            $availableTimes[] = date('g:i A', $current_time);
+            list($hours, $minutes, $seconds) = explode(':', $default_time_settings["TimeIncrement"]);
+            $elapsed_seconds = ($hours * 3600) + ($minutes * 60) + $seconds;
+            $current_time += $elapsed_seconds;
+        }
+        return array($availableTimes, $default_time_settings["StartDate"], $default_time_settings["EndDate"], $default_time_settings["TimeIncrement"], $default_time_settings['TimeId']);
     }
 
     /**
