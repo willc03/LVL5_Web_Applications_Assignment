ba9575e72de7e9aa7b0dfa9d4ed0a2bd0acf63f6
willcorkill13 <corkillw@gmail.com>
2023-04-24 17:29:43 +0100

    Begin to create contents

--- app/Views/pages/admin/manage_golf.php
+++ app/Views/pages/admin/manage_golf.php
@@ -1,1 +1,6 @@
-<?php
+<h1>Golf Management</h1>
+<br>
+<div class="introduction">
+    <p>This facility will allow you to manage the times at which users are able to book golf on days you set.</p>
+    <p>Please note that the default times cannot be removed. Days which don't have times set will use these times.</p>
+</div>
